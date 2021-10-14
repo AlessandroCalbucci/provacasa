ad1f411477eb69cd03474cd5a1878d11a160e1aa
AlessandroCalbucci <alessandro.calbucci.stud@ispascalcomandini.ir>
2021-10-14 14:46:50 +0200

    v1

---
+++ provagitcasa/provagitcasa/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace provagitcasa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 10;
+            int n1 = 11;
+
+            int somma = n + n1;
+
+            Console.WriteLine(somma);
+            Console.ReadLine();
+        }
+    }
+}
